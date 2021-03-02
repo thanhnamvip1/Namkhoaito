fa3b12103be2c6dc2a2b0b94a5193108f5ffb0ab
thanhnamvipvip1 <thanhnamvipvipvip@gmail.com>
2021-03-02 16:10:59 +0700

    nam

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -6,7 +6,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
-echo Pass: Thuonghai001
+echo Pass: Thanhnam001
 curl -O https://raw.githubusercontent.com/ThuongHai/windows-rdp/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
