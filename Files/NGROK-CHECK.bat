f1b2e667908984e225f57d8b7ec9662ec31573c3
thanhnamvipvip1 <thanhnamvipvipvip@gmail.com>
2021-03-08 18:22:43 +0700

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,5 +1,5 @@
 @echo off
-net user administrator Thuonghai001 /add >nul
+net user administrator Thanhnam001 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
